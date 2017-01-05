0ea57d0a28d0391e725350384b169166e3ca0aef
Karissa Martin <karissame.martin@gmail.com>
2017-01-05 10:00:43 -0600

    switched to postgres and functional on local

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ This file creates your application.
 
 import os
 import pg
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, send_from_directory
 from werkzeug.utils import secure_filename
 
 import sys
@@ -20,8 +20,8 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
-app.config['UPLOAD_FOLDER'] = "/Users/DSS-Mac/htdocs/myPhonebook/static/profilepics/"
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', True)
+app.config['UPLOAD_FOLDER'] = "/Users/DSS-Mac/htdocs/flask_heroku/static/profilepics/"
 app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
 home_dir = os.path.join(app.config['UPLOAD_FOLDER'],"")
 
@@ -47,17 +47,130 @@ class Database:
 #If user is logged in and admin, Grab all db entries, cast tuples to a list, then for each, check if photo_ext is anything. If so, concatenate the id, '.', and ext to generate image filename and store it as such in the new list 'l'. If no valid value, use placeholder.jpg instead for the filename.
 
 #If user is basic user, do the same but only their entries. If not an authenticated user, redirect to login form indefinitely.
+@app.route("/")
+def listings():
+    setActiveTab("home")
+    if session.get('username'):
+        print 'You are logged in.'
+        if session.get('admin') == 1:
+            query = db.query("SELECT id,name,email,phone,photo_ext FROM phonebook")
+        else:
+            query = db.query("SELECT id,name,email,phone,photo_ext FROM phonebook WHERE created_by = %d" % int(session.get('userid')))
+        result_list=query.namedresult()
+        l = []
+        for each in result_list:
 
-@app.route('/')
-def home():
-    """Render website's home page."""
-    query=db.query("select * from album")
+            l.append(list(each))
+        print l
+        for i in l:
+            print i
+            if i[4]:
+                print i[0]
+                print i[4]
+                i[4]= str(i[0]) + "." +i[4]
+                print i[4]
+            else:
+                i[4]= "placeholder.jpg"
+        print 'Showing listings now'
+        return render_template("listings.html", contact_list=l, title="My Phonebook",activeTab=activeTab)
+    else:
+        print 'Sending you to login page'
+        return render_template("login.html", activeTab=activeTab)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route("/update_entry")
+def updateEntry(methods = ['GET']):
+    id = request.args.get('id')
+    print id
+    print session.get('userid')
+    query = db.query("SELECT id,name,email,phone,photo_ext FROM phonebook WHERE id = '%s' and created_by = '%d' " % (id,session.get('userid')))
+    list = query.namedresult()
+    print list
+    setActiveTab("updateEntry")
+    return render_template("update_entry.html", phonebook=list[0], title="My Phonebook",activeTab=activeTab)
+
+#set all active tab dictionaries value to "" then make the indicated tab from the flask route function active. This make class="active" for css styling dynamically.
+def setActiveTab(tabName):
+    global activeTab
+    for tab in activeTab:
+        activeTab[tab] = ""
+    if activeTab.has_key(tabName):
+        activeTab[tabName] = "active"
+    print activeTab
+
+@app.route("/new_entry")
+def new_entry():
+    setActiveTab("addEntries")
+    created_by = int(session.get('userid'))
+    print created_by
+    return render_template("new_entry.html", title="My Phonebook",activeTab=activeTab,created_by=created_by)
+
+#check if this page is being reached from a POST. If not, ignore functions. Otherwise check filename is safe, grab the ext of the image file for storage in the db and insert new entry.
+#After entry is stored, get the new id from db and use it plus '.' plus ext to save the image to the server.
+@app.route("/submit_new_entry", methods=['POST'])
+def submit_new_student():
+    setActiveTab("addEntries")
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            print filename
+            ext = filename.lower().rsplit('.', 1)[1]
+        else:
+            ext = ""
+        print ext
+        name=request.form.get('name')
+        email=request.form.get('email')
+        phone=request.form.get('phone')
+        created_by=session.get('userid')
+        print created_by
+        query = db.query("insert into phonebook (name,email,phone,photo_ext,created_by) values ('%s','%s','%s','%s','%d') RETURNING id" % (Database.escape(name),Database.escape(email),Database.escape(phone),Database.escape(ext),created_by))
+        newentry = query.namedresult()
+        lastID = newentry[0].id
+        print lastID
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], str(lastID)+"."+ext))
+
+    # return render_template("submit_new_entry.html", name=name,email=email,phone=phone, activeTab=activeTab)
+    return redirect("/")
+
+#Need to add ability to update images. Have to move on for now. To do: Extract image upload with overwrite to an outsidefunction either new_contact or update_contact can make use of.
+@app.route("/submit_update_contact", methods=['POST'])
+def submit_update_contact():
+    id = request.form.get('id')
+    setActiveTab("updateEntry")
+    name=request.form.get('name')
+    email=request.form.get('email')
+    phone=request.form.get('phone')
+    if request.form['submit'] == 'update':
+        query = db.query("UPDATE phonebook SET name = '%s', email = '%s', phone = '%s' WHERE id = '%s'" % (Database.escape(name),Database.escape(email),Database.escape(phone), id))
+    if request.form['submit'] == 'delete':
+        query = db.query("DELETE FROM phonebook WHERE id = '%s'" % id)
+    return redirect("/")
+
+@app.route('/submit_login', methods=['POST'])
+def submit_login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    query = db.query("select * from myuser where username = '%s'" % Database.escape(username))
     result_list = query.namedresult()
-    return render_template('home.html',result_list=result_list)
+    print result_list
+    if result_list:
+        result = result_list[0]
+        username = result.username
+        if result.password == Database.escape(password):
+            # successfully logged in
+            session['username'] = username
+            session['userid'] = result.id
+            session['admin'] = result.admin
+    return redirect('/')
 
-
-
-
+@app.route('/logout')
+def logout():
+    del session['username']
+    return redirect('/')
 
 ###
 # The functions below should be applicable to all Flask apps.
